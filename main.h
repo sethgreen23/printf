85760bc70b827662228d8b88615c100316ab9ec6
Mahmoud Ehab <eng.mahmoud.e.hussein@gmail.com>
2023-09-17 22:19:52 +0300

    Third task

--- main.h
+++ main.h
@@ -12,7 +12,7 @@ int _printf(const char *format, ...);
 int print_s(char *str);
 int _strlen(char *s);
 int print_d(int n);
-int print_b(int n);
+int print_b(unsigned int n);
 int choose_func(char ch, va_list lst);
 int print_percent(int num);
 int print_ns(char ch);
