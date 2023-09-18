9e19d52d157c5816b9ae3401fec8f9cf49d7e5f2
sethgreen23 <dridi.chaith@gmail.com>
2023-09-18 12:55:31 -0700

    change main func

--- main.c
+++ main.c
@@ -18,5 +18,27 @@ int main(void)
 	printf("[length: %d]\n", n);
 	n = _printf("%b\n", -100);
 	printf("[length: %d]\n", n);
+	n = _printf("%x\n", 0);
+	printf("[length: %d]\n", n);
+	n = _printf("%x\n", 100);
+	printf("[length: %d]\n", n);
+	n = _printf("%X\n", 100);
+	printf("[length: %d]\n", n);
+	n = printf("%x\n", -100);
+	printf("[length: %d]\n", n);
+	n = _printf("%X\n", -100);
+	printf("[length: %d]\n", n);
+	n = _printf("%x\n", 500);
+	printf("[length: %d]\n", n);
+	n = _printf("%X\n", 500);
+	printf("[length: %d]\n", n);
+	n = _printf("%x\n", INT_MAX);
+	printf("[length: %d]\n", n);
+	n = _printf("%X\n", INT_MAX);
+	printf("[length: %d]\n", n);
+	n = _printf("%x\n", INT_MIN);
+	printf("[length: %d]\n", n);
+	n = _printf("%X\n", INT_MIN);
+	printf("[length: %d]\n", n);
 	return (0);
 }
