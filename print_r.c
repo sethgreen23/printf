ed3d0247c838e949ee6a1f382986089e8bfbe3fb
sethgreen23 <dridi.chaith@gmail.com>
2023-09-19 16:04:19 -0700

    alter print_r

--- print_r.c
+++ print_r.c
@@ -8,16 +8,15 @@
 int print_r(va_list lst)
 {
 	int len, j;
-	char *str;
+	char *str = va_arg(lst, char *);
 
-	str = va_arg(lst, char *);
 	if (str == NULL)
 	{
 		write(1, "(null)", 6);
 		return (6);
 	}
 	len = _strlen(str);
-	for (j = len - 1; j <= 0; j--)
+	for (j = len - 1; j >= 0; j--)
 		write(1, &str[j], 1);
 	return (len);
 }
