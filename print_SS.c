cf5c0ba35b35e8fb5c5d773bc82b4a484bac7a1c
Mahmoud Ehab <eng.mahmoud.e.hussein@gmail.com>
2023-09-20 00:37:59 +0300

    Edit print_SS.c

--- print_SS.c
+++ print_SS.c
@@ -16,8 +16,6 @@ int print_S(va_list lst)
 		write(1, "(null)", 6);
 		return (6);
 	}
-	if (*str == "")
-		return (-1);
 	for (; *str != '\0'; str++)
 	{
 		if ((*str >= 0 && *str < 32) || (*str >= 127))
