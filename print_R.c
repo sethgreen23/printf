89062a5f298a7ea37763b7317568e2fc745b2b90
sethgreen23 <dridi.chaith@gmail.com>
2023-09-19 16:56:07 -0700

    print_R check NULL)

--- print_R.c
+++ print_R.c
@@ -11,7 +11,12 @@ int print_R(va_list lst)
 	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 	char *str = va_arg(lst, char *);
-
+	
+	if (str = NULL)
+	{
+		write(1, "(null)", 6);
+		return (6);
+	}
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		for (ii = 0; ii < 54; ii++)
