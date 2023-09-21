25fa8538b5c869623eb93f2677e8e2201be2bbc5
Mahmoud Ehab <eng.mahmoud.e.hussein@gmail.com>
2023-09-21 14:38:34 +0300

    Print Rot13

--- main.c
+++ main.c
@@ -8,29 +8,11 @@ int main(void)
 {
 	int n;
 
-	n = _printf("%x\n", 0);
-	printf("%d [length: %d]\n", 0, n);
-	n = _printf("%x\n", 100);
-	printf("%d [length: %d]\n", 100, n);
-	n = _printf("%X\n", 100);
-	printf("%d [length: %d]\n", 100, n);
-	n = printf("%x\n", -100);
-	printf("%d [length: %d]\n", -100, n);
-	n = _printf("%X\n", -100);
-	printf("%d [length: %d]\n", -100, n);
-	n = _printf("%x\n", 500);
-	printf("%d [length: %d]\n", 500, n);
-	n = _printf("%X\n", 500);
-	printf("%d [length: %d]\n", 500, n);
-	n = _printf("%x\n", INT_MAX);
-	printf("%d [length: %d]\n", INT_MAX, n);
-	n = _printf("%X\n", INT_MAX);
-	printf("%d [length: %d]\n", INT_MAX, n);
-	n = _printf("%x\n", INT_MIN);
-	printf("%d [length: %d]\n", INT_MIN, n);
-	n = _printf("%X\n", INT_MIN);
-	printf("%d [length: %d]\n", INT_MIN, n);
-	n = _printf("%S", "Best\tSchool");
-	printf("\n%d", n);
+	n = _printf("%R", "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+	printf("\n%d\n", n);
+	n = _printf("%R", "nopqrstuvwxyzabcdefghijklm");
+	printf("\n%d\n", n);
+	n = _printf("%R", "65123945");
+	printf("\n%d\n", n);
 	return (0);
 }
--- print_R.c
+++ print_R.c
@@ -8,32 +8,30 @@
 int print_R(va_list lst)
 {
 	int i, len = 0;
-	char *str_clone;
-	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 	char *str = va_arg(lst, char *);
 
-	if (str == NULL)
-	{
-		write(1, "(null)", 6);
-		return (6);
-	}
-	str_clone = str;
-	for (; *str_clone != '\0'; ++str_clone)
-	{
-		for (i = 0; input[i] != '\0'; ++i)
-		{
-			if (input[i] == *str_clone)
-			{
-				*str_clone = output[i];
-				break;
-			}
-		}
-	}
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		write(1, &str[i], 1);
-		len++;
+		if ((str[i] >= 'a' && str[i] <= 'm') || (str[i] >= 'A' && str[i] <= 'M'))
+		{
+			putchar(str[i] + 13);
+			len++;
+		}
+		else if (str[i] >= 'n' && str[i] <= 'z')
+		{
+			putchar(str[i] - 13);
+			len++;
+		}
+		else if (str[i] >= 'N' && str[i] <= 'Z')
+		{
+			putchar(str[i] - 13);
+			len++;
+		}
+		else
+		{
+			putchar(str[i]);
+			len++;
+		}
 	}
 	return (len);
 }
